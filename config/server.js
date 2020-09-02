29cc3e37ba52a304a297b5bc907ac30860b051dd
Francisco Peralta <franp@mantik.tech>
2020-09-02 19:40:22 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6546b0a08d7cec0aa62c1db036976341'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e02b266b-0386-4356-9e0c-a4f656613418'
+};
